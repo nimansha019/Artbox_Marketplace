577069eaefcee7366200b5d335a399bbad60519e
nimansha019 <112418501+nimansha019@users.noreply.github.com>
2025-09-24 19:47:00 +0530

    commit -items

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -10,7 +10,7 @@ const BestSeller = () => {
 
   return (
     <div className="mt-16 px-4">
-      <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
+      <p className="text-2xl md:text-3xl font-medium">Best Seller items</p>
       <div className="flex flex-wrap gap-4 mt-4">
         {bestSellers.map((item) => (
           <ProductCard key={item._id} product={item} />
